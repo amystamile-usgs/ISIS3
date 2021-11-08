f1df740b5e589a1437c24b12ac16923a4d9277b1
acpaquette <acp263@nau.edu>
2021-11-08 13:43:18 -0700

    Adds the ability to adjust precision to q apps that display DNs

--- isis/src/base/objs/SpecialPixel/SpecialPixel.h
+++ isis/src/base/objs/SpecialPixel/SpecialPixel.h
@@ -383,7 +383,7 @@ namespace Isis {
    *
    * @return string The name of the pixel type
    */
-  inline QString PixelToString(double d) {
+  inline QString PixelToString(double d, double precision=8) {
     if(Isis::IsSpecial(d)) {
       if(Isis::IsNullPixel(d)) return "Null";
       if(Isis::IsLrsPixel(d)) return "Lrs";
@@ -394,7 +394,7 @@ namespace Isis {
     }
 
     QString result;
-    return result.setNum(d, 'g', 8);
+    return result.setNum(d, 'g', precision);
   }
 
 
